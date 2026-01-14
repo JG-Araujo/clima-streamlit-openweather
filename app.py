e4ae7cd252dd5bc74ab558d2ec08fbce5be32a61
Abimael Oliveira <112417460+abimaelProgammer@users.noreply.github.com>
2026-01-13 22:27:38 -0300

    criação da tela clima atual
    foi criado a tela principal com clima atual, e todas as informações pertinentes ao clima do momento da solicitação

--- app.py
+++ app.py
@@ -3,45 +3,58 @@ import pandas as pd
 import plotly.express as px
 import filtros as f
 from util import carregar_cidades
+import api_clima as ac
+import time
 
 # 1. Configuração da página (título na aba do navegador)
 st.set_page_config(page_title="Clima Hoje", page_icon=":sun_with_face:", layout="wide")
 # 2. Título e Subtítulo na página
 st.title("Clima Hoje :sun_with_face:")
 st.write("Veja as métricas de clima")
+st.caption("Escolha uma cidade na barra lateral para ver o clima atual e a previsão.")
 # 3. Criando dados de exemplo (Simulando um Excel/CSV)
+
 dados = {
-    'Produto': ['Notebook', 'Mouse', 'Teclado', 'Monitor', 'Cadeira', 'Headset'],
-    'Vendas': [15000, 2000, 3500, 8000, 5000, 1200],
-    'Quantidade': [5, 40, 20, 10, 8, 15],
-    'Categoria': ['Eletrônicos', 'Acessórios', 'Acessórios', 'Eletrônicos', 'Móveis', 'Acessórios']
+    'Cidade': carregar_cidades()
 }
+
 df = pd.DataFrame(dados)
-cidades = carregar_cidades()
+opcoes = ['selecione uma cidade'] + df['Cidade'].unique().tolist()
 
 # 4. Barra Lateral (Sidebar) para Filtros
-f.filtros_sidebar(cidades)
-
-# Filtrando o DataFrame baseado na escolha
-categoria_selecionada = st.sidebar.selectbox(
-    "Selecione a Categoria",
-    options=df["Categoria"].unique()
+st.sidebar.header("Filtros")
+cidade_selecionada = st.sidebar.selectbox(
+    "selecione uma cidade",
+    options=opcoes,
 )
+# Filtrando o DataFrame baseado na escolha
+if cidade_selecionada == 'selecione uma cidade':
+    st.info("Por favor, selecione uma cidade para ver o clima.")
+else:
+    df_filtrado = f.previsao(cidade_selecionada)
 
-# 5. Exibindo Métricas (KPIs)
-col1, col2 = st.columns(2)
-df_filtrado = df[df["Categoria"] == categoria_selecionada]
-total_vendas = df_filtrado['Vendas'].sum()
-total_qtd = df_filtrado['Quantidade'].sum()
+   # Métricas do dia atual (primeira linha do df FILTRADO)
+    col1, col2, col3 = st.columns(3)
+    cidade = df_filtrado['Cidade'][0]
+    Temp = df_filtrado["Temperatura"][0]
+    Sensacao = df_filtrado["Sensação Térmica"][0]
+    Umidade = df_filtrado["Umidade"][0]
+    Descricao = df_filtrado["Descrição"][0]
+    Vento = df_filtrado["Velocidade do Vento"][0]
 
-col1.metric("Faturamento Total", f"R$ {total_vendas:,.2f}")
-col2.metric("Itens Vendidos", total_qtd)
+    hora = time.strftime("%H:%M:%S", time.localtime())
+    st.write(f"Atualizado às: {hora}")
+    col1.metric("CIDADE", f"{cidade}", f"{int(Temp)}°C")
+    col2.metric("SENSAÇÃO TÉRMICA", f"{int(Sensacao)}°C")
+    col2.metric("UMIDADE", f"{int(Umidade)}%")
+    col3.metric("DESCRIÇÃO", f"{Descricao}")
+    col3.metric("VELOCIDADE DO VENTO", f"{int(Vento)} m/s")
 
-# 6. Exibindo o Gráfico com Plotly
-st.subheader(f"Gráfico de Vendas - {categoria_selecionada}")
-fig = px.bar(df_filtrado, x='Produto', y='Vendas', color='Produto', title="Vendas por Produto")
-st.plotly_chart(fig, use_container_width=True)
+    # # 6. Exibindo o Gráfico com Plotly
+    # st.subheader(f"Gráfico de Vendas - {categoria_selecionada}")
+    # fig = px.bar(df_filtrado, x='Produto', y='Vendas', color='Produto', title="Vendas por Produto")
+    # st.plotly_chart(fig, use_container_width=True)
 
-# 7. Exibindo os dados brutos (opcional)
-if st.checkbox("Mostrar tabela de dados"):
-    st.dataframe(df_filtrado)
+    # 7. Exibindo os dados brutos (opcional)
+    if st.checkbox("Mostrar tabela de dados"):
+            st.dataframe(df_filtrado)
--- filtros.py
+++ filtros.py
@@ -1,14 +1,37 @@
 import streamlit as st
 import pandas as pd
-cities_filter = []
+import api_clima as ac
 def filtros_sidebar(df: pd.DataFrame):
-    st.sidebar.header("Filtros (Drill-down)")
+    # st.sidebar.header("Filtros (Drill-down)")
     global cities_filter
 # filtro Cidade
-    city_option = df["name"].tolist()
+    city_option = df["name"].tolist() 
     cities_filter = st.sidebar.multiselect(
         "Cidades",
         options=city_option,
+        max_selections=1,
         default=[]
     )
+
+def previsao(cidade: list) -> pd.DataFrame:
+    lat,lon = ac.obter_coordenadas(cidade)
+    clima_atual = ac.obter_clima_atual(lat, lon)
+    dados = {'Cidade': [cidade],
+             'Temperatura': [clima_atual['temperatura']],
+             'Sensação Térmica': [clima_atual['sensacao']],
+             'Umidade': [clima_atual['umidade']],
+             'Descrição': [clima_atual['descricao']],
+             'Velocidade do Vento': [clima_atual['vento']]
+
+        }
+    df_clima = pd.DataFrame(dados)
+    return df_clima 
+
+
+
+def aplicar_filtros(df_base: pd.DataFrame) -> pd.DataFrame:
+    df_f = df_base.copy()
+    if cities_filter:
+        df_f = df_f[df_f["name"].isin(cities_filter)]
+    return df_f
 

ee1802debcf6cca965b824edf4d730c2382e7f16
J. Gabriel Araújo <jgabriel.araujo0408@gmail.com>
2026-01-18 15:41:21 -0300

    excluídas funções desnecessárias

--- util.py
+++ util.py
@@ -1,34 +1,5 @@
 import pandas as pd
 import streamlit as st
-import json
-
-def padronizar_colunas(df: pd.DataFrame) -> pd.DataFrame:
-    """Ajustes leves: tipos e colunas auxiliares."""
-    df = df.copy()
-
-    # Garantir que colunas existam (para evitar erros em datasets similares)
-    colunas_esperadas = ['name', 'country']
-    faltantes = [c for c in colunas_esperadas if c not in df.columns]
-    if faltantes:
-        st.error(f"Colunas faltantes no CSV: {faltantes}")
-        st.stop()
-    return df
-
-def carregar_cidades() -> list:
-    """Carrega a lista de cidades a partir de um arquivo CSV."""
-    try:
-        df = pd.read_json("city.list.json")
-        df = padronizar_colunas(df)
-        return df['name'].tolist(), df['country'].tolist(), df['coord'].tolist()
-    except FileNotFoundError:
-        st.error(f"Arquivo não encontrado: cities_list.csv ")
-        st.stop()
-    except pd.errors.EmptyDataError:
-        st.error("Arquivo CSV está vazio.")
-        st.stop()
-    except Exception as e:
-        st.error(f"Erro ao carregar o arquivo CSV: {e}")
-        st.stop()
 
 def traduzir_aqi(aqi_valor):
     """
